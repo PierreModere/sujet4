fbde59dc43d9710c18207f2422441fdc99ecbf23
PierreModere <45337625+PierreModere@users.noreply.github.com>
2020-11-22 03:11:00 +0100

    Ajout de la sphere

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect } from 'react'
 
-import Cube from "./Styles/Cube";
-import Button from "./Styles/Button";
-import { Canvas } from "react-three-fiber";
+import Cube from "./Styles/Cube"
+import Button from "./Styles/Button"
+import { Canvas } from 'react-three-fiber'
 
-import { stateInit, reducer } from "./reducers/cube";
+import { stateInit, reducer } from "./reducers/cube"
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, stateInit);
-  const { cubes, number, easter_egg } = state;
+  const [state, dispatch] = useReducer(reducer, stateInit)
+  const { cubes, number, easter_egg } = state
 
   useEffect(() => {
-    if (number === easter_egg) {
-      dispatch({ type: "EASTER_EGGS", number: number });
+    if( number === easter_egg ){
+      dispatch({type :'EASTER_EGGS', number : number})
     }
-  }, [number]);
+  }, [number])
 
   return (
     <>
-      <Button onClick={() => dispatch({ type: "ADD_CUBE" })}>ADD CUBE</Button>
+      <Button onClick={() => dispatch({ type: "ADD_CUBE" })}>
+        ADD CUBE
+      </Button>
       <Button primary onClick={() => dispatch({ type: "SHUFFLE" })}>
         SHUFFLE
       </Button>
       <Button primary onClick={() => dispatch({ type: "STOP_ODD" })}>
-        STOP ODD NUMBER
+        START ANIM ODD NUMBER
       </Button>
       <Button primary onClick={() => dispatch({ type: "CHANGE_ODD" })}>
-        START ODD NUMBER
+        STOP ANIM ODD NUMBER
       </Button>
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         {cubes.length > 0 &&
-          cubes.map((cube, i) => (
-            <Cube
-              key={i}
-              {...cube}
-              easter_egg={easter_egg}
-              position={[-3.5 + 1.1 * (i % 7), 2 - 1.1 * Math.floor(i / 7), 0]}
-            />
-          ))}
-      </Canvas>
+          cubes.map((cube, i) => <Cube key={i} {...cube } easter_egg={ easter_egg } position={[-3.5 + 1.1* (i % 7), 2 - 1.1 * (Math.floor(i / 7)) , 0]} />)}
+          </Canvas>
     </>
   );
 }
--- src/Styles/Cube.js
+++ src/Styles/Cube.js
@@ -3,28 +3,24 @@ import { useFrame } from "react-three-fiber";
 
 function Cube({ position, color, number, easter_egg, stop }) {
   const mesh = useRef();
-  const [hovered, setHover] = useState(false);
+  const [setHover] = useState(false);
+
+  useFrame(() => {
+    if (stop === true) {
+      mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    }
+  });
 
   if (easter_egg === number)
     return (
-      <mesh
-        position={position}
-        ref={mesh}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-      >
-        <sphereBufferGeometry args={[0.5, 10, 10]} />
+      <mesh position={position} ref={mesh}>
+        <sphereBufferGeometry args={[0.5, 0, 0]} />
         <meshStandardMaterial color={color} />
       </mesh>
     );
   else
     return (
-      <mesh
-        position={position}
-        ref={mesh}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-      >
+      <mesh position={position} ref={mesh}>
         <boxBufferGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color={color} />
       </mesh>
